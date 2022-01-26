79f830443301bab062ee20227e6684314d023c6a
blvkroot <hsalukamba17@gmail.com>
2022-01-27 00:51:04 +0100

    GameSearchWords removed.

--- src/main/java/api/tistech/services/GameSearchService/ISearchWordsService.java
+++
@@ -1,8 +0,0 @@
-package api.tistech.services.GameSearchService;
-
-import api.tistech.dto.NodesTreeDTO;
-import api.tistech.dto.ThirdIntGeneratorDTO;
-
-public interface ISearchWordsService {
-    public String execute(NodesTreeDTO nodes);
-}
